0dea3556c3db6ce65c1a7e9a443be676ecedecb9
znh <znh@znh>
2023-11-20 21:03:50 +0800

    feat: 优化代码

--- handler/user.py
+++ handler/user.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from middleware.unify_json import unify_json
 from models.user import User
 from schemas.index import Message
 from store.mysql.dependencies import get_db_session
@@ -10,5 +11,10 @@ router = APIRouter()
 
 
 @router.get("/", response_model=Message)
-def user(db: AsyncSession = Depends(get_db_session)) -> Message:
-    select(User).where('user_name = sss')
+@unify_json
+async def user(db: AsyncSession = Depends(get_db_session)) -> Message:
+    stmt = select(User).where(User.name == "spongebob")
+    result = await db.execute(stmt)
+    print(result)
+    msg = Message(message='hello world')
+    return msg
--- models/user.py
+++ models/user.py
@@ -1,6 +1,10 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
 from models.model import BaseModel
 
 
 class User(BaseModel):
     __tablename__ = "user"
-    pass
+    name: Mapped[str] = mapped_column(String(30))
+    password: Mapped[str] = mapped_column(String(100))
